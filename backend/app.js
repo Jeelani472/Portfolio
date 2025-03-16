fb8a6a19b251c163107a27995801e13664a33aac
Jeelani472 <jeelanmohammed529@gmail.com>
2025-03-17 01:18:00 +0530

    Added a Project Files

--- backend/app.js
+++ backend/app.js
@@ -65,9 +65,9 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../frontend")));
 
-// Serve static files from the "public" folder
-app.use(express.static('public'));
+
 
 // MongoDB Connection using environment variable
 const mongoURI = process.env.MONGODB_URI;
